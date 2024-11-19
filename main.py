27949b82b89dd1288a051d6a08f0aad3a51be1c9
Jingyi (Claire) Xu <75157518+xuclaire0234@users.noreply.github.com>
2024-11-18 22:53:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@
 
 Image classification of Chest X Rays in one of three classes: Normal, Viral Pneumonia, COVID-19
 
-Notebook created for the guided project [Detecting COVID-19 with Chest X Ray using PyTorch](https://www.coursera.org/projects/covid-19-detection-x-ray) on Coursera
-
 Dataset from [COVID-19 Radiography Dataset](https://www.kaggle.com/tawsifurrahman/covid19-radiography-database) on Kaggle
 
 # Importing Libraries
